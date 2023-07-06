3316df0cc084c949b3505e5b8ee10c9e5a30fae8
ayusekar94 <ayu.wiranti181@gmail.com>
2023-07-06 17:58:39 +0700

    syc

--- resources/views/pages/modal_add.blade.php
+++ resources/views/pages/modal_add.blade.php
@@ -6,37 +6,10 @@
           <h5 class="modal-title" id="exampleModalLabel">Tambah Kegiatan</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-        <form action="/kegiatan" method="post" enctype="multipart/form-data"> @csrf <div class="modal-body">
-          {{-- <div class="form-group mb-2">
-            <label class="from-label">Name</label>
-            <div class="col-sm-9">
-              <input type="text"
-                  class="form-control"
-                  name="name" placeholder="Nama Lengkap"> @error('name') <code>
-                    {{ $message }}
-                  </code> @enderror
-            </div>
-          </div>  
+        <form action="/kegiatan" method="post" enctype="multipart/form-data"> @csrf 
+          <div class="modal-body">
           <div class="form-group mb-2">
-            <label class="from-label">Tanggal</label>
-            <div class="col-sm-9">
-              <input type="date"
-                  class="form-control" name="tgl" placeholder="Tanggal"> @error('tgl') <code>
-                    {{ $message }}
-                  </code> @enderror
-            </div>
-          </div>  
-          <div class="form-group mb-2">
-            <label class="from-label">Kegiatan</label>
-            <div class="col-sm-9">
-              <input type="text"
-                  class="form-control" name="kegiatan" placeholder="Kegiatan"> @error('kegiatan') <code>
-                    {{ $message }}
-                  </code> @enderror
-            </div>
-          </div> --}}
-          <div class="form-group mb-2">
-              <label class="form-label">Nama</label>
+              <label class="form-label">Nama Kegiatan</label>
               <div class="col-sm-9">
                 <input type="text" name="name" placeholder="Nama Lengkap" class="form-control" /> @error('nama') <code>
                   {{ $message }}
@@ -52,7 +25,7 @@
               </div>
             </div>
             <div class="form-group mb-2">
-              <label class="col-sm-3 col-form-label">Kegiatan</label>
+              <label class="col-sm-3 col-form-label">Deskripsi Kegiatan</label>
               <div class="col-sm-9">
                 <input type="text" name="kegiatan" class="form-control" /> @error('kegiatan') <code>
                   {{ $message }}
